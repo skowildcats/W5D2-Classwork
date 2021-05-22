9f532ed77645efe336ccd7869acc78890e2beb2f
Jon Chen <jonathan.chen.6799@gmail.com>
2021-05-22 14:15:39 -0500

    Finished countries bonus

--- skeleton/lib/04a_countries_bonus.rb
+++ skeleton/lib/04a_countries_bonus.rb
@@ -14,6 +14,14 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT name
+    FROM countries
+    WHERE gdp > 
+      (SELECT MAX(gdp)
+      FROM countries
+      WHERE continent = 'Europe'
+      GROUP BY continent)
+
   SQL
 end
 
@@ -21,6 +29,12 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT continent, name, area
+  FROM countries
+  WHERE area IN
+    (SELECT MAX(area)
+    FROM countries
+    GROUP BY continent)
   SQL
 end
 
@@ -28,5 +42,22 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT
+      A.name,
+      A.continent
+    FROM
+      countries A
+    WHERE
+      A.population > 3 * (
+        SELECT
+          MAX(B.population)
+        FROM
+          countries B
+        WHERE
+          A.name != B.name -- got me!
+            AND A.continent = B.continent
+      )
+
+
   SQL
 end
