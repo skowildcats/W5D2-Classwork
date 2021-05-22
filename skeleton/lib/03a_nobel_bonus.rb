d60eb89f032e0ee536a92c21d6ad06bcd1beb24c
Jon Chen <jonathan.chen.6799@gmail.com>
2021-05-22 13:37:05 -0500

    Finished nobel bonus problem

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -11,5 +11,17 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT yr
+    FROM nobels
+    WHERE yr IN 
+      (SELECT yr
+      FROM nobels
+      WHERE subject = 'Physics'
+      GROUP BY yr) 
+    AND yr NOT IN (
+      SELECT yr
+      FROM nobels
+      WHERE subject = 'Chemistry'
+      GROUP BY yr)
   SQL
 end
